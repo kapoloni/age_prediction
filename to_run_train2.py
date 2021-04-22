6e7199597f48b3fea4060517ca91f637377b108c
kapoloni <katiampoloni@gmail.com>
2021-04-22 13:35:30 +0000

    changes in train

--- to_run_train2.py
+++ to_run_train2.py
@@ -10,8 +10,7 @@ age = '[70,100]'
 snapshot = {'L': 'outputs/ckpt_16-04-2021_age_[0-70]_RMS_wd_0_L_dp0.4.pth.tar',
             'R': 'outputs/ckpt_16-04-2021_age_[0-70]_RMS_wd_0_R_dp0.5.pth.tar'}
 
-clr = {'L_RMS': '[-7.,-3.5]', 'R_RMS': '[-7.,-3.5]'}
-# dp = {'L': '0.4', 'R': '0.3'}
+clr = {'L_RMS': '[-4.35,-4.1]', 'R_RMS': '[-7.,-3.5]'}
 
 for dp in ['0.1', '0.2', '0.3', '0.4', '0.5']:
     for side in ['L', 'R']:
@@ -25,8 +24,8 @@ for dp in ['0.1', '0.2', '0.3', '0.4', '0.5']:
             wd = '0'
 #             # Train
             train = './train.py --num_epochs 150 --batch_size 512 --loss MSE ' +\
-                         '--side ' + side + ' --age_range ' + age + ' --optimizer ' + \
-                         optimizer + ' --weight_decay ' + wd +\
+                    '--side ' + side + ' --age_range ' + age + ' --optimizer ' + \
+                    optimizer + ' --weight_decay ' + wd +\
                          ' --clr ' + clr[side+"_"+optimizer] + \
                          ' --snapshot ' + snapshot[side] + " --dropout_rate " + dp
             print(train)
